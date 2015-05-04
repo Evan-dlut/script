4841ccba195a4889f34a967d19d11cb1032a26e0
evan.Home.PC <evanyu.dlut@gmail.com>
2015-05-05 00:19:38 +0800

    delete useless code

--- python/pa.py
+++ python/pa.py
@@ -13,28 +13,33 @@ if __name__ == '__main__':
 	host = 'http://s.hc360.com/?w=%BA%DA%C1%FA%BD%AD&mc=enterprise'
 	f = open('a.txt','a')
 	i=0
-	for x in xrange(78,500):
-		print x
-		message = copy(host+'&e='+str(x), r'<h3><a data-exposurelog="([^"]+)"')
-		for x in message:
-			x = 'http://' + x + '.b2b.hc360.com/shop/company.html'
-			m = copy(x, r'<title>([^\<]+)</title>')
-			print str(i)+' '+m[0]
-			if len(m[0]) == 18:
-				continue
-			pattern = re.compile(r'([\d]+)')
-			this = pattern.findall(m[0])
-			if this:
-				pass
-			else:
-				continue
-			i=i+1
-			try:
-				f.write(x+' '+m[0])
-				f.write('\n')
-			except IOError:
-				print 'IOError'
-				f = open('a.txt','a')
-			else:
-				pass
+	for x in xrange(1,500):
+		try:
+			print x
+			message = copy(host+'&e='+str(x), r'<h3><a data-exposurelog="([^"]+)"')
+			for x in message:
+				x = 'http://' + x + '.b2b.hc360.com/shop/company.html'
+				m = copy(x, r'<title>([^\<]+)</title>')
+				print str(i)+' '+m[0]
+				if len(m[0]) == 18:
+					continue
+				pattern = re.compile(r'([\d]+)')
+				this = pattern.findall(m[0])
+				if this:
+					pass
+				else:
+					continue
+				i=i+1
+				try:
+					f.write(x+' '+m[0])
+					f.write('\n')
+				except IOError:
+					print 'IOError'
+					f = open('a.txt','a')
+				else:
+					pass
+			
+		except Exception, e:
+			continue
 	f.close()
+		
